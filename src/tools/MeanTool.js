d005031b888e0f32fe44590bff4df8978b6ed14a
sdpeyton <stephendpeyton@gmail.com>
2017-10-21 17:06:40 -0400

    minor update to make sure area calculation tools don't show too many significant digits

--- src/tools/MeanTool.js
+++ src/tools/MeanTool.js
@@ -60,12 +60,14 @@ class MeanTool extends React.Component {
             if (event_type === 'rectangle') {
                 let latlngs = layer.getBounds();
                 let coors = [latlngs.getWest(), latlngs.getSouth(), latlngs.getEast(), latlngs.getNorth()];
-                value = gio.mean(Map.image, coors).join(', ');
+                value = gio.mean(Map.image, coors)
+                    .map(value => value.toFixed(2)).join(', ');
                 Map.stop_draw_rectangle();
             } else {
                 let geojson = layer.toGeoJSON();
                 let coors = geojson.geometry.coordinates;
-                value = gio.mean(Map.image, geojson).join(', ');
+                value = gio.mean(Map.image, geojson)
+                    .map(value => value.toFixed(2)).join(', ');
                 Map.stop_draw_polygon();
             }
 
