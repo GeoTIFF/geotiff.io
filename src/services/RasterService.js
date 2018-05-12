853621a473aea9035ab3cc2e3f33d58414fbc5aa
= <steve.peyton@arcadiapower.com>
2018-05-12 10:34:27 -0400

    add different resolutions based on screen size

--- src/services/RasterService.js
+++ src/services/RasterService.js
@@ -2,6 +2,17 @@ import geoblaze from 'geoblaze';
 import GeoRasterLayer from 'georaster-layer-for-leaflet';
 import chroma from 'chroma-js';
 
+const getResolution = () => {
+  const width = document.documentElement.clientWidth;
+  if (width < 900) {
+    return Math.pow(2, 5);
+  } else if (width < 1200) {
+    return Math.pow(2, 6);
+  } else {
+    return Math.pow(2, 7);
+  }
+}
+
 const RasterService = {
 
   create_raster(input) {
@@ -10,7 +21,8 @@ const RasterService = {
         .then(georaster => {
           let options = {
             georaster: georaster,
-            opacity: 0.7
+            opacity: 0.7,
+            resolution: getResolution(),
           };
           let raster = new GeoRasterLayer(options);
           resolve(raster);
