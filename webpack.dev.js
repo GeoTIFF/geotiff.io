fac578fd2067442c41e4197cf9cba36cc5d2087e
sdpeyton <steve.peyton@arcadiapower.com>
2018-07-25 07:48:59 -0400

    webpack prod

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,15 +1,9 @@
 const webpack = require('webpack');
+const merge = require('webpack-merge');
 const path = require('path');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const shared = require('./webpack.shared');
 
-module.exports = {
-  entry: [
-    'babel-polyfill',
-    './src/index.js'
-  ],
+const dev = {
   mode: 'development',
   devtool: 'cheap-module-eval-source-map',
   devServer: {
@@ -22,40 +16,9 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     publicPath: '/'
   },
-  resolve: {
-    modules: ['node_modules', 'src'],
-    extensions: ['.js', '.json']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.less$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "less-loader"
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      }
-    ]
-  },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new CopyWebpackPlugin([
-      {
-        from: './public/images',
-        to: 'images',
-      }
-    ]),
-    new FaviconsWebpackPlugin('./public/favicon.png'),
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-      title: 'GeoTIFF.io',
-    }),
     new webpack.HotModuleReplacementPlugin()
   ]
-}
+};
+
+module.exports = merge(shared, dev);
---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const path = require('path');
+const shared = require('./webpack.shared');
+
+const prod = {
+  mode: 'production',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/'
+  },
+};
+module.exports = merge(shared, prod);
